0292b94d25ac19f9a8230b96ac74df5e85dbcbf9
Tatsuya Tsuda <game.world@hotmail.co.jp>
2012-08-23 18:48:35 +0900

    updated

--- scenes/game/player.rb
+++ scenes/game/player.rb
@@ -8,10 +8,13 @@ class Player
 	attr_accessor :items
   attr_accessor :hp
   attr_accessor :pos
-  attr_accessor :img
+	attr_accessor :img
 	
+	DIVISION = 4.0
+	START_X = 391
+	START_Y = 541
 	
-  def initialize(map, x = 391, y = 541)
+  def initialize(map, x = START_X, y = START_Y)
 		@map = map
 		@age = 0
 		@items = Array.new(ITEMS.length) { 0 }
@@ -23,17 +26,33 @@ class Player
     img_file = File.join(File.dirname(__FILE__), "..", "..", "images", "men0.png")
     @img = Image.load(img_file)
     @step = 0.0625
-    @hp = 100
+    @hp = 100.00
   end
-
+	
+	def animation
+		@x_next, @y_next = @map.get_point_pos(@pos.to_i , @route_no)
+	
+		dx = @x_next - @x
+		dy = @y_next - @y
+	
+		w = @pos / @step % 1.0 
+		if w % DIVISION == 0
+			@x += dx / DIVISION
+			@y += dy / DIVISION	
+		end
+	end
+	
   def move(counter, route_no)
 		@route_no = route_no 
-    @x, @y = @map.get_point_pos(@pos.to_i, @route_no)
+    
+		self.animation
+		#@x, @y = @map.get_point_pos(@pos.to_i, @route_no)
 		@pos += @step
 		
+		#端まで来たら初期化する
 		if @pos.to_i >= @map.points[@route_no].size
-			@x = 391
-			@y = 541
+			@x = START_X
+			@y = START_Y
 			@pos = 0.0
       return counter - @step.abs, true
     end
@@ -61,7 +80,6 @@ class Player
 		end
 		img_file = File.join(File.dirname(__FILE__), "..", "..", "images", char_img)
     @img = Image.load(img_file)
-
 	end
 	
   def draw
