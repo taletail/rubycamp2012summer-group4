7be60f93b61addaf7dc1b0ecbe96adc86af6aab5
Kana Uchida <game.world@hotmail.com>
2012-08-24 08:57:17 +0900

    updated

--- scenes/title.rb
+++ scenes/title.rb
@@ -3,8 +3,10 @@
 class Title
   def initialize
     bg_file = File.join(File.dirname(__FILE__), "..", "images", "start.png")
+		music = File.join(File.dirname(__FILE__), "..", "sound", "opening.wav")
     @background_img = Image.load(bg_file)
-
+		@opening_music = Sound.new(music)
+		@played = 0
     @pushed_space = false
     @alpha = 255
   end
@@ -13,6 +15,11 @@ class Title
     if Input.keyPush?(K_SPACE)
       @pushed_space = true
     end
+		
+		if @played == 0
+			@opening_music.play
+			@played = 1
+		end
 
     if @pushed_space
       @alpha -= 7
