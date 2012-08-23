bbdba7aebe76c2d469047d9de0d8b50efe686968
Kana Uchida <game.world@hotmail.com>
2012-08-23 17:05:57 +0900

    updated.

--- scenes/title.rb
+++ scenes/title.rb
@@ -4,12 +4,27 @@ class Title
   def initialize
     bg_file = File.join(File.dirname(__FILE__), "..", "images", "start.png")
     @background_img = Image.load(bg_file)
+
+    @pushed_space = false
+    @alpha = 255
   end
 
   def play
-    Window.draw(100, 0, @background_img)
     if Input.keyPush?(K_SPACE)
-      Scene.set_scene(:game)
+      @pushed_space = true
+    end
+
+    if @pushed_space
+      @alpha -= 4
+      if @alpha <= 0
+        @alpha = 0
+      end
+      Window.drawAlpha(100, 0, @background_img, @alpha)
+      if @alpha <= 0
+        Scene.set_scene(:game)
+      end
+    else
+      Window.draw(100, 0, @background_img)
     end
   end
 end
