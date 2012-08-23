cbb1e9261d06c0827eb4f166f41301479b9dc934
Ryuta Yoshii <game.world@hotmail.co.jp>
2012-08-23 16:24:06 +0900

    updated

--- scenes/ending.rb
+++ scenes/ending.rb
@@ -12,14 +12,19 @@ class Ending
 			case @age
 				when 0
 					bg_file = File.join(File.dirname(__FILE__),"..","images", "end0sai.png")#whenの部分に年齢を書く。年齢に応じた画像に書き換える。
+					end_sound = File.join(File.dirname(__FILE__),"..","sound", "end0.wav")
 				when 1..20
 					bg_file = File.join(File.dirname(__FILE__),"..","images", "end20sai.png")
+					end_sound = File.join(File.dirname(__FILE__),"..","sound", "end20.wav")
 				when 21..50
 					bg_file = File.join(File.dirname(__FILE__),"..","images", "end40sai.png")
+					end_sound = File.join(File.dirname(__FILE__),"..","sound", "end40.wav")
 				when 51..80
 					bg_file = File.join(File.dirname(__FILE__),"..","images", "6080sai.png")
+					end_sound = File.join(File.dirname(__FILE__),"..","sound", "end60.wav")
 				else
 					bg_file = File.join(File.dirname(__FILE__),"..","images", "90sai.png")
+						end_sound = File.join(File.dirname(__FILE__),"..","sound", "end90.wav")
 			end
 			@ending = Image.load(bg_file)
 		else
@@ -27,6 +32,8 @@ class Ending
 				Image.load(File.join(File.dirname(__FILE__),"..","images", "oki0#{i}.jpg"))
 			}
 			@counter = 0
+			@end_se  =Sound.load(end_sound)
+			@sound_played = 0
 		end
 	end
 
@@ -40,12 +47,16 @@ class Ending
 	end
   
 	def play
-        if Input.keyPush?(K_RETURN)
+    if Input.keyPush?(K_RETURN)
 			exit
-        end
+    end
 		if @won
-            Window.draw(0,0,@ending)
-        else
+      Window.draw(0,0,@ending)
+			if @sound_played == 0
+				Sound.play(@end_se)
+				@sound_played = 1
+			end
+    else
 			@counter += 1
 			animation
 		end
